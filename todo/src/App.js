fce170d7f866954926db8c293e3c679ba1e2ac93
Alexey10031994 <alexseva94@gmail.com>
2022-12-04 18:12:43 +0200

    create-react-app

---
+++ todo/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" >
+
+        Добро Пожаловать !
+
+      </div>
+
+    )
+
+  }
+}
+
+export default App;
