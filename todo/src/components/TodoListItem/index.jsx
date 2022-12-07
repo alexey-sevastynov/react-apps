5313c9407ab5ac55bbf0c9fb84299fab99484bc5
Alexey10031994 <alexseva94@gmail.com>
2022-12-07 22:48:03 +0200

    changes component "TodoListItem" from functions to class

--- todo/src/components/TodoListItem/index.jsx
+++ todo/src/components/TodoListItem/index.jsx
@@ -1,39 +1,38 @@
 import React from "react";
-
-import { fa } from 'react-bootstrap-icons';
-
 import './todoListItem.css';
 
-const TodoListItem = ({ label, important = false }) => {
+class TodoListItem extends React.Component {
 
-    const style = {
-        color: important ? 'steelblue' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
-    };
+    render() {
+        const { label, important = false } = this.props;
 
-    return (
-        <span className="todo-list-item" >
-            <span className="todo-list-item-label"
-                style={style}>
-                {label}
+        const style = {
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
+        };
+
+        return (
+            <span className="todo-list-item" >
+                <span className="todo-list-item-label"
+                    style={style}>
+                    {label}
+                </span>
+
+                <div>
+                    <button type="button"
+                        className="btn btn-outline-danger btn-sm float-right">
+                        <i className="bi bi-trash" />
+                    </button>
+                    <button type="button"
+                        className="btn btn-outline-success btn-sm float-right">
+                        <i className="bi bi-exclamation-lg"></i>
+                    </button>
+                </div>
             </span>
+        )
+    }
+}
 
-            <div>
-                <button type="button"
-                    className="btn btn-outline-danger btn-sm float-right">
-                    <i className="bi bi-trash" />
-                </button>
-                <button type="button"
-                    className="btn btn-outline-success btn-sm float-right">
-                    <i className="bi bi-exclamation-lg"></i>
-                </button>
-            </div>
-
-
-
-        </span>
-    )
-};
 
 
 export default TodoListItem;
