0a43c60f3b106fe0247f09b24e62a4e777a66309
Alexey10031994 <alexseva94@gmail.com>
2022-12-07 22:53:47 +0200

    changes component "ItemStatusFiter" from functions to class

--- todo/src/components/ItemStatusFilter/index.jsx
+++ todo/src/components/ItemStatusFilter/index.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 
-function ItemStatusFilter() {
-    return (
-        <div className='d-flex flex-row '>
-            <button type='button'
-                className='btn btn-info'>All</button>
-            <button type='button'
-                className='btn btn-outline-secondary'>Active</button>
-            <button type='button'
-                className='btn btn-outline-secondary'>Done</button>
-        </div>
-    )
+class ItemStatusFilter extends React.Component {
+
+
+    render() {
+
+        return (
+            <div className='d-flex flex-row '>
+                <button type='button'
+                    className='btn btn-info'>All</button>
+                <button type='button'
+                    className='btn btn-outline-secondary'>Active</button>
+                <button type='button'
+                    className='btn btn-outline-secondary'>Done</button>
+            </div>
+        )
+    }
 }
+
+
+
 
 export default ItemStatusFilter;
