ea459d4f5d63ecba86b25b0808e9bc84a81397d1
Alexey10031994 <alexseva94@gmail.com>
2022-12-12 22:28:44 +0200

    added form

--- todo/src/components/AddItem/index.jsx
+++ todo/src/components/AddItem/index.jsx
@@ -3,14 +3,42 @@ import './addItem.css';
 
 class AddItem extends React.Component {
 
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (event) => {
+        this.setState(({ label }) => {
+            return {
+                label: event.target.value
+            }
+        })
+        console.log(this.state.label);
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.props.addItem(this.state.label);
+    }
+
     render() {
         return (
-            <div className='addItem'>
+            <form
+                className='addItemForm'
+                onSubmit={this.onSubmit}
+            >
+
+                <input
+                    type="text"
+                    className='form-control'
+                    placeholder='what need to do?'
+                    onChange={this.onLabelChange}
+                />
                 <button
                     className='btn btn-outline-secondary'
-                    onClick={() => this.props.addItem('Hello World!')}
+                    // onClick={() => this.props.addItem('Hello World!')}
                 >Add Item</button>
-            </div>
+            </form>
         )
     }
 
